30baf9bfb4e45e9fc171a80da14b4ca49131a135
saptak dutta <duttasaptak79@gmail.com>
2026-04-27 12:56:05 +0530

    create the api-error class , api-response and mail sending utils functions

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer"
+import Mailgen from "mailgen"
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
@@ -10,5 +11,53 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+let mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+        // Appears in header & footer of e-mails
+        name: 'Mailgen',
+        link: 'https://mailgen.js/'
+    }
+});
+
+const sendmail = async function(options){
+    
+ let  emailText = mailGenerator.generatePlaintext(options.verificationEmail);
+ let emailHtml = mailGenerator.generate(options.verificationEmail);
+  const mail = {
+     from: 'mail.taskmanager@example.com',
+    to:options.email,
+    subject: options.subject,
+    text: emailText,// plain‑text body
+    html: emailHtml, // HTML body
+  }
+  try{
+   await  transporter.sendMail(mail);
+  }catch(err){
+    console.lo
+  }
+
+}
 
 
+
+const verificationEmail = (username ,verificationURL) => {
+    return {
+         body: {
+        name: username,
+        intro: 'Welcome to Mailgen! We\'re very excited to have you on board.',
+        action: {
+            instructions: 'Verify your Email',
+            button: {
+                color: '#22BC66', // Optional action button color
+                text: 'Confirm your account',
+                link: verificationURL,
+            }
+        },
+        outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+    }
+    }
+};
+
+
+export {sendmail,verificationEmail}
